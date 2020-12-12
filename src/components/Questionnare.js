7733edd69c95cd8d5067d729e6032aeccead301e
atlroc99 <tyaniceluv89@gmail.com>
2020-12-11 22:59:53 -0500

    fixed padding

--- src/components/Questionnare.js
+++ src/components/Questionnare.js
@@ -76,7 +76,6 @@ const Questionnare = () => {
     <fieldset>
     <h5 className="title"><i className="fas fa-list-alt"></i>  System Configuration (WebCTRL and I-Vu)</h5>
      <Form className="p-4">
-     </Form>
 
      <div style={{color:'Red',textAlign:'center'}}>{errMsg}</div>
 
@@ -95,157 +94,157 @@ const Questionnare = () => {
             </Form.Group>
           </div>
 
-<hr className="border"></hr>
+      <hr className="border"></hr>
 
-          <Form.Group>
-          <Form.Label>What version of WebCTRL or I-Vu are you currently running?</Form.Label>
-          <Form.Control className="inputBox" 
-                type="text" 
-                value={webCtrlVersion}
-                onChange = {(e)=>setWebCtrlVersion(e.target.value)}
-                placeholder="Enter Your Answer" />
-          </Form.Group>
-
-          <Form.Group>
-          <Form.Label>What is the IP of the WebCTRL or I-Vu server?</Form.Label>
-          <Form.Control className="inputBox" 
-                type="text" 
-                value ={webCtrlIp}
-                onChange={(e)=>setWebCtrlIp(e.target.value)}
-                placeholder="Enter Your Answer" />
-          </Form.Group>
-
-          <Form.Group>
-          <Form.Label>List all add-ons that are enabled (not built-in)</Form.Label>
-          <Form.Control className="inputBox" 
-                type="text" 
-                value ={addOnsList}
-                onChange={(e)=>setAddOnsList(e.target.value)}
-                placeholder="Enter Your Answer" />
-          </Form.Group>
-
-          <Form.Group>
-          <Form.Label>List any add-ons not currently running that are desired in the next 3-6 months?</Form.Label>
-          <Form.Control className="inputBox" 
-                type="text" 
-                value={oldAddOns}
-                onChange={(e)=>setOldAddOns(e.target.value)}
-                placeholder="Enter Your Answer"/>
-          </Form.Group>
-
-          <Form.Group>
-          <Form.Label>What alert notification methods are used?</Form.Label>
-          <Form.Control className="inputBox" 
-                type="text" 
-                value={alertNotificationMethod}
-                onChange={(e)=>setAlertNotificationMethod(e.target.value)}
-                placeholder="Enter Your Answer" />
-          </Form.Group>
-
-          <Form.Group id="formGridCheckbox">
-          <Form.Label>Do you depend on specific From: addresses in notifications?</Form.Label>
-          <Form.Check 
-              type="checkbox" 
-              value={addressesInNotification}
-              onClick={()=>setAddressesInNotification('Yes')}
-              label="Yes"/>
-              <Form.Check 
-              type="checkbox" 
-              value={addressesInNotification}
-              onClick={()=>setAddressesInNotification('No')}
-              label="No"/>
-        </Form.Group>
-
-        <Form.Group>
-        <Form.Label>What brand database are you using and what version?</Form.Label>
-        <Form.Control className="inputBox" 
-              type="text" 
-              value={dbBrandAndVersion}
-              onChange={(e)=>setDbBrandAndVersion(e.target.value)}
-              placeholder="Enter Your Answer" />
-        </Form.Group>
-
-        <Form.Group>
-        <Form.Label>Are you using Hierarchical servers?</Form.Label>
-        <Form.Check 
-            type="checkbox" 
-            value={usingHierarchialServer}
-            onClick={()=>setUsingHierarchialServer('Yes')}
-            label="Yes"/>
-            <Form.Check 
-            type="checkbox" 
-            value={usingHierarchialServer}
-            onClick={()=>setUsingHierarchialServer('No')}
-            label="No"/>
-        </Form.Group>
-
-        <h5 className="text text-info">Size of Environment : </h5>
-        <Form.Row>
-            <Form.Group as={Col} controlId="formGridState">
-              <Form.Label>System directory size (bytes):</Form.Label>
-              <Form.Control className="inputBox" 
-                    type="text"
-                    value={directorySize} 
-                    onChange={(e)=>setDirectorySize(e.target.value)}
-                    placeholder="Enter Your Answer" />
-            </Form.Group>
-        
-            <Form.Group as={Col} controlId="formGridState">
-              <Form.Label>Total Database size (bytes):</Form.Label>
+              <Form.Group>
+              <Form.Label>What version of WebCTRL or I-Vu are you currently running?</Form.Label>
               <Form.Control className="inputBox" 
                     type="text" 
-                    value={dbSize}
-                    onChange={(e)=>setDbSize(e.target.value)}
+                    value={webCtrlVersion}
+                    onChange = {(e)=>setWebCtrlVersion(e.target.value)}
                     placeholder="Enter Your Answer" />
+              </Form.Group>
+
+              <Form.Group>
+              <Form.Label>What is the IP of the WebCTRL or I-Vu server?</Form.Label>
+              <Form.Control className="inputBox" 
+                    type="text" 
+                    value ={webCtrlIp}
+                    onChange={(e)=>setWebCtrlIp(e.target.value)}
+                    placeholder="Enter Your Answer" />
+              </Form.Group>
+
+              <Form.Group>
+              <Form.Label>List all add-ons that are enabled (not built-in)</Form.Label>
+              <Form.Control className="inputBox" 
+                    type="text" 
+                    value ={addOnsList}
+                    onChange={(e)=>setAddOnsList(e.target.value)}
+                    placeholder="Enter Your Answer" />
+              </Form.Group>
+
+              <Form.Group>
+              <Form.Label>List any add-ons not currently running that are desired in the next 3-6 months?</Form.Label>
+              <Form.Control className="inputBox" 
+                    type="text" 
+                    value={oldAddOns}
+                    onChange={(e)=>setOldAddOns(e.target.value)}
+                    placeholder="Enter Your Answer"/>
+              </Form.Group>
+
+              <Form.Group>
+              <Form.Label>What alert notification methods are used?</Form.Label>
+              <Form.Control className="inputBox" 
+                    type="text" 
+                    value={alertNotificationMethod}
+                    onChange={(e)=>setAlertNotificationMethod(e.target.value)}
+                    placeholder="Enter Your Answer" />
+              </Form.Group>
+
+              <Form.Group id="formGridCheckbox">
+              <Form.Label>Do you depend on specific From: addresses in notifications?</Form.Label>
+              <Form.Check 
+                  type="checkbox" 
+                  value={addressesInNotification}
+                  onClick={()=>setAddressesInNotification('Yes')}
+                  label="Yes"/>
+                  <Form.Check 
+                  type="checkbox" 
+                  value={addressesInNotification}
+                  onClick={()=>setAddressesInNotification('No')}
+                  label="No"/>
             </Form.Group>
-        </Form.Row>
 
-        <h5 className="text text-info">System Settings : System Statistics :</h5>
-        <Form.Row>
-        <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Number of controllers:</Form.Label>
-          <Form.Control className="inputBox" 
-                type="text" 
-                value={controllerNo}
-                onChange={(e)=>setControllerNo(e.target.value)}
-                placeholder="Enter Your Answer" />
-        </Form.Group>
-    
-         <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Number of points:</Form.Label>
-          <Form.Control className="inputBox" 
-                type="text" 
-                value={pointsNo}
-                onChange={(e)=>setPointsNo(e.target.value)}
-                placeholder="Enter Your Answer" />
-        </Form.Group>
-        
-         <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Number of Sources:</Form.Label>
-          <Form.Control className="inputBox" 
-                type="text" 
-                value={sourceNo}
-                onChange={(e)=>setSourceNo(e.target.value)}
-                placeholder="Enter Your Answer" />
-        </Form.Group>
-        
-         <Form.Group as={Col} controlId="formGridEmail">
-          <Form.Label>Number of Samples:</Form.Label>
-          <Form.Control className="inputBox" 
-                type="text"
-                value={sampleNo} 
-                onChange={(e)=>setSampleNo(e.target.value)}
-                placeholder="Enter Your Answer" />
-        </Form.Group>
-      </Form.Row>
+            <Form.Group>
+            <Form.Label>What brand database are you using and what version?</Form.Label>
+            <Form.Control className="inputBox" 
+                  type="text" 
+                  value={dbBrandAndVersion}
+                  onChange={(e)=>setDbBrandAndVersion(e.target.value)}
+                  placeholder="Enter Your Answer" />
+            </Form.Group>
 
-      <div style={{marginLeft:'45%'}}>
-      <Button type="submit" onClick={submit} variant="info" style={{width:'200px',margin:'5px'}}>Submit</Button>
-      <Button type="reset" onClick={reset} variant="secondary" style={{width:'200px'}}>Reset</Button>
-      </div>
-      
-     </fieldset>
-     </div>
+            <Form.Group>
+            <Form.Label>Are you using Hierarchical servers?</Form.Label>
+            <Form.Check 
+                type="checkbox" 
+                value={usingHierarchialServer}
+                onClick={()=>setUsingHierarchialServer('Yes')}
+                label="Yes"/>
+                <Form.Check 
+                type="checkbox" 
+                value={usingHierarchialServer}
+                onClick={()=>setUsingHierarchialServer('No')}
+                label="No"/>
+            </Form.Group>
+
+            <h5 className="text text-info">Size of Environment : </h5>
+            <Form.Row>
+                <Form.Group as={Col} controlId="formGridState">
+                  <Form.Label>System directory size (bytes):</Form.Label>
+                  <Form.Control className="inputBox" 
+                        type="text"
+                        value={directorySize} 
+                        onChange={(e)=>setDirectorySize(e.target.value)}
+                        placeholder="Enter Your Answer" />
+                </Form.Group>
+            
+                <Form.Group as={Col} controlId="formGridState">
+                  <Form.Label>Total Database size (bytes):</Form.Label>
+                  <Form.Control className="inputBox" 
+                        type="text" 
+                        value={dbSize}
+                        onChange={(e)=>setDbSize(e.target.value)}
+                        placeholder="Enter Your Answer" />
+                </Form.Group>
+            </Form.Row>
+
+            <h5 className="text text-info">System Settings : System Statistics :</h5>
+              <Form.Row>
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>Number of controllers:</Form.Label>
+                  <Form.Control className="inputBox" 
+                        type="text" 
+                        value={controllerNo}
+                        onChange={(e)=>setControllerNo(e.target.value)}
+                        placeholder="Enter Your Answer" />
+                </Form.Group>
+            
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>Number of points:</Form.Label>
+                  <Form.Control className="inputBox" 
+                        type="text" 
+                        value={pointsNo}
+                        onChange={(e)=>setPointsNo(e.target.value)}
+                        placeholder="Enter Your Answer" />
+                </Form.Group>
+                
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>Number of Sources:</Form.Label>
+                  <Form.Control className="inputBox" 
+                        type="text" 
+                        value={sourceNo}
+                        onChange={(e)=>setSourceNo(e.target.value)}
+                        placeholder="Enter Your Answer" />
+                </Form.Group>
+                
+                <Form.Group as={Col} controlId="formGridEmail">
+                  <Form.Label>Number of Samples:</Form.Label>
+                  <Form.Control className="inputBox" 
+                        type="text"
+                        value={sampleNo} 
+                        onChange={(e)=>setSampleNo(e.target.value)}
+                        placeholder="Enter Your Answer" />
+                </Form.Group>
+              </Form.Row>
+
+              <div style={{marginLeft:'45%'}}>
+                <Button type="submit" onClick={submit} variant="info" style={{width:'200px',margin:'5px'}}>Submit</Button>
+                <Button type="reset" onClick={reset} variant="secondary" style={{width:'200px'}}>Reset</Button>
+              </div>
+          </Form>
+      </fieldset>
+    </div>
   )
 }
 export default Questionnare
