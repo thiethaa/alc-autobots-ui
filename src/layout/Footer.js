e50f86c03eb48acdcb78f44e946dc38ae83b1fab
atlroc99 <tyaniceluv89@gmail.com>
2020-12-13 13:29:36 -0500

    updated

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className = "footerBox">
             <br></br>
             <br></br>
-            <p className="footerTag" style={{textAlign:'center'}}>A Carrier Company &#169; Carrier 2020 All rights reserved.</p>
+            <p className="footerTag" style={{textAlign:'center'}}>A Carrier Company &#169; Carrier 2020     All rights reserved.</p>
         </footer>
     );
 }
